1e17a60f8ff1bc35430212b51ab37207626c51d8
YatharthVyas <yatharth2000.vyas@gmail.com>
2020-03-05 22:58:48 +0530

    Add Navbar

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app.register_blueprint(user)
 app.register_blueprint(post)
 
 
-@app.route('/')
-def home():
+@app.route('/ide')
+def ide():
     return render_template('home.html', langs=langs)
 
 
